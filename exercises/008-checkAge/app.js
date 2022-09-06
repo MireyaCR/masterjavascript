8b34c92480f263931c5809cda5acd91bdaa6d01b
MireyaCR <mireya.mcr@gmail.com>
2022-09-06 20:38:57 +0000

    E008

--- exercises/008-checkAge/app.js
+++ exercises/008-checkAge/app.js
@@ -1,3 +1,12 @@
 function checkAge(name, age) {
   // your code here
+  var mensaje = {};
+  if (age >= 21) {
+    mensaje = 'Welcome, ' + name + '!';
+  } else {
+    mensaje = 'Go home, ' + name + '!';
+  }
+  return (mensaje);
 }
+let output = checkAge('Adrian', 21);
+console.log(output); // --> '¡Welcome Adrian!'
