3e71ff485a9053c4bcfbb37efc72c6775ee4a82b
MireyaCR <mireya.mcr@gmail.com>
2022-09-06 19:33:52 +0000

    E007

--- exercises/007-removeProperty/app.js
+++ exercises/007-removeProperty/app.js
@@ -1,3 +1,10 @@
 function removeProperty(obj, key) {
   // your code here
+  delete obj[key];
 }
+let obj = {
+  name: 'Sam',
+  age: 20
+}
+removeProperty(obj, 'name');
+console.log(obj.name); // --> undefined
