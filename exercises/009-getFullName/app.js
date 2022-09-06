66bce73012497303233f20380da1fc5ca399a156
MireyaCR <mireya.mcr@gmail.com>
2022-09-06 20:49:34 +0000

    E009

--- exercises/009-getFullName/app.js
+++ exercises/009-getFullName/app.js
@@ -1,3 +1,8 @@
 function getFullName(firstName, lastName) {
   // your code here
+  var mensaje = {};
+  mensaje = firstName + ' ' + lastName;
+  return (mensaje);
 }
+let output = getFullName('Joe', 'Smith');
+console.log(output); // --> 'Joe Smith'
