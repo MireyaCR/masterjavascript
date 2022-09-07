8af7d095e516ff37d5b18bab9c9d617564d39692
MireyaCR <mireya.mcr@gmail.com>
2022-09-07 16:50:51 +0000

    E033

--- exercises/033-isPersonOldEnoughToDrinkAndDrive/app.js
+++ exercises/033-isPersonOldEnoughToDrinkAndDrive/app.js
@@ -1,5 +1,5 @@
 function isPersonOldEnoughToDrinkAndDrive(person) {
   // the person object contains an "age" property inside
   // Add your code after this line
-  
+  return person.age >=21 && person.age>=16;
 }
