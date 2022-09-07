7885b6f02797a40cc64491ffd5735807eb9e6ddc
MireyaCR <mireya.mcr@gmail.com>
2022-09-07 18:45:25 +0000

    E047

--- exercises/047-computePerimeterOfACircle/app.js
+++ exercises/047-computePerimeterOfACircle/app.js
@@ -1,4 +1,6 @@
 function computePerimeterOfACircle(radius) {
   // your code here
-
+return 2*Math.PI*radius;
 }
+let output = computePerimeterOfACircle(4);
+console.log(output); // --> 25.132741228718345
