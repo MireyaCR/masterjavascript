f67a110847148cb9e596c3d9239ab99ab17e6859
MireyaCR <mireya.mcr@gmail.com>
2022-09-08 19:58:17 +0000

    E071

--- exercises/071-removeStringValuesLongerThan/app.js
+++ exercises/071-removeStringValuesLongerThan/app.js
@@ -1,4 +1,18 @@
 function removeStringValuesLongerThan(num, obj) {
     // your code here
-
+    const propiedades = Object.keys(obj)
+  
+    for (let key of propiedades) {
+        if (typeof obj[key]== "string" && obj[key].length >num) {
+          delete obj[key];
+          }
+      }
+      return obj;
 }
+let obj = {
+    name: 'Montana',
+    age: 20,
+    location: 'Texas'
+  };
+  removeStringValuesLongerThan(6, obj);
+  console.log(obj); // { age: 20, location: 'Texas' }
