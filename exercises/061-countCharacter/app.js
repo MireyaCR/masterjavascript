b0e4c3c582b17f07bf881faaf49485dedbe0c2ba
MireyaCR <mireya.mcr@gmail.com>
2022-09-08 18:26:06 +0000

    E061

--- exercises/061-countCharacter/app.js
+++ exercises/061-countCharacter/app.js
@@ -1,4 +1,11 @@
 function countCharacter(str, char) {
     // your code here
     
+    let contador = 0;
+    for (let index = 0; index < str.length; index++) {
+        const element = str[index];
+        if (element===char)
+        contador++;
+    }
+    return contador;
 }
