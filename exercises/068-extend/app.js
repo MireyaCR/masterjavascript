3f0643cf058226902abe17934a6b38501e0cc846
MireyaCR <mireya.mcr@gmail.com>
2022-09-08 19:16:50 +0000

    E068

--- exercises/068-extend/app.js
+++ exercises/068-extend/app.js
@@ -9,5 +9,13 @@ let obj2 = {
 
 function extend(obj1, obj2) {
     // your code here
-
+    const keysObj1 = Object.keys(obj1)
+    const keysObj2 = Object.keys(obj2)
+  
+    for (let key of keysObj2) {
+      if (!keysObj1.includes(key)) {
+        obj1[key] = obj2[key]
+        }
+    }
+    return obj1;
 }
