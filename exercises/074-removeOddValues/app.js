a44038a2f2f318537885624a665aee9601024d8d
MireyaCR <mireya.mcr@gmail.com>
2022-09-09 07:01:23 +0000

    E074

--- exercises/074-removeOddValues/app.js
+++ exercises/074-removeOddValues/app.js
@@ -1,4 +1,11 @@
 function removeOddValues(obj) {
     // your code here
-
+    const propiedades = Object.keys(obj)
+  
+    for (let key of propiedades) {
+        if (obj[key]%2!=0) {
+          delete obj[key];
+          }
+      }
+      return obj;
 }
