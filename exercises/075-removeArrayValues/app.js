60545cdca872b6da85525efdf5483059d4ca4844
MireyaCR <mireya.mcr@gmail.com>
2022-09-09 17:04:41 +0000

    E074

--- exercises/075-removeArrayValues/app.js
+++ exercises/075-removeArrayValues/app.js
@@ -1,4 +1,19 @@
 function removeArrayValues(obj) {
     // your code here
     
+    const propiedades = Object.keys(obj)
+  
+    for (let key of propiedades) {
+        if (obj[key]==[]) {
+          delete obj[key];
+          }
+      }
+      return obj;
 }
+let obj = {
+    a: [1, 3, 4],
+    b: 2,
+    c: ['hi', 'there']
+  }
+  removeArrayValues(obj);
+  console.log(obj); // --> { b: 2 }
