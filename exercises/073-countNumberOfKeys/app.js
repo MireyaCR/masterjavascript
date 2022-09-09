ca6e04c37f1eb8bfedb6719280bd788260060a0c
MireyaCR <mireya.mcr@gmail.com>
2022-09-09 06:59:09 +0000

    E073

--- exercises/073-countNumberOfKeys/app.js
+++ exercises/073-countNumberOfKeys/app.js
@@ -1,4 +1,17 @@
 function countNumberOfKeys(obj) {
     // your code here
-
+   // let size=0;
+   // for(let k in obj) {
+    //size++;
+ // }
+//return size;
+return Object.keys(obj).length;
 }
+let obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  //d: 4,
+};
+let output = countNumberOfKeys(obj);
+console.log(output); // --> 3
