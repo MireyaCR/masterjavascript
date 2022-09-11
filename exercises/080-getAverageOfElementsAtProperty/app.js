cba4fce1172b44cdc8cc7adf4bd9fa2127c3bc4a
MireyaCR <mireya.mcr@gmail.com>
2022-09-11 08:25:56 +0000

    E080

--- exercises/080-getAverageOfElementsAtProperty/app.js
+++ exercises/080-getAverageOfElementsAtProperty/app.js
@@ -1,1 +1,20 @@
 // Write your function here
+function getAverageOfElementsAtProperty(obj,key) {
+    var sum=0;
+    var i=0;
+    if (obj[key] && typeof obj[key]=== "object" && obj[key].length) {
+        while (i < obj[key].length) {            
+             sum += obj[key][i];
+             i++;
+        }
+        console.log(sum)
+        sum = sum/obj[key].length;
+    }
+    return sum;
+}      
+
+let obj = {
+    key: [1, 2, 3]
+  };
+  let output = getAverageOfElementsAtProperty(obj, 'key');
+  console.log(output); // --> 2
