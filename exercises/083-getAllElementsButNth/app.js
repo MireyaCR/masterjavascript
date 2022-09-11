f7ace7db58d27fd6b32cd673bf16f2f00e3ba97c
MireyaCR <mireya.mcr@gmail.com>
2022-09-11 16:12:59 +0000

    E083

--- exercises/083-getAllElementsButNth/app.js
+++ exercises/083-getAllElementsButNth/app.js
@@ -1,6 +1,7 @@
 function getAllElementsButNth(array, n) {
-    // Write your function here
-
+    // Write your function here  
+    let nuevo= array.splice(n,1);
+    return array;
 }
 let output = getAllElementsButNth(['a', 'b', 'c'], 1);
 console.log(output); // --> ['a', 'c']
