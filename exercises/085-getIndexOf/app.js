00bbea7fbe22ec7c29c1098fd31900354f8bf79b
MireyaCR <mireya.mcr@gmail.com>
2022-09-11 17:18:37 +0000

    E085

--- exercises/085-getIndexOf/app.js
+++ exercises/085-getIndexOf/app.js
@@ -1,1 +1,12 @@
 // Write your function here
+function getIndexOf(caracter, cadena) {
+    let posi = -1;
+    for (let i = 0; i < cadena.length; i++) {
+        if (cadena[i] === caracter && posi===-1) {
+            posi = i;
+        }        
+    }
+    return posi;
+}
+let output = getIndexOf('a', 'I am a hacker');
+console.log(output); // --> 2
