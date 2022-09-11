ffff4f7e5ae7703ed1bb5fa079605fcf2ffa7166
MireyaCR <mireya.mcr@gmail.com>
2022-09-11 17:57:24 +0000

    E087

--- exercises/086-findMinLengthOfThreeWords/app.js
+++ exercises/086-findMinLengthOfThreeWords/app.js
@@ -1,1 +1,10 @@
 // Write your function here
+function findMinLengthOfThreeWords(str1,str2,str3) {
+    let uno = str1.length;
+    let dos = str2.length;
+    let tres = str3.length;
+    let menor = Math.min(uno,dos,tres);
+    return menor;
+}
+let output = findMinLengthOfThreeWords('a', 'be', 'see');
+console.log(output); // --> 1
--- exercises/087-findMaxLengthOfThreeWords/app.js
+++ exercises/087-findMaxLengthOfThreeWords/app.js
@@ -1,1 +1,8 @@
 // Write your function here
+function findMaxLengthOfThreeWords(str1,str2,str3) {
+    let uno = str1.length;
+    let dos = str2.length;
+    let tres = str3.length;
+    let mayor = Math.max(uno,dos,tres);
+    return mayor;
+}
