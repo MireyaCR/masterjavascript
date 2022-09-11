65b9dfb44f59678ffdbb490fd84f188ead5d6a37
MireyaCR <mireya.mcr@gmail.com>
2022-09-11 18:41:25 +0000

    E088

--- exercises/088-Select/app.js
+++ exercises/088-Select/app.js
@@ -1,1 +1,18 @@
 // Write your function here
+function select(arr,obj) {
+    let nobj = {};
+       for(let key of arr) {
+          if (obj[key]) {
+            nobj[key] = obj[key];
+            }
+        }
+        return nobj;
+    }
+    let arr = ['c', 'f'];
+  let obj = {
+    a: 5,
+    b: 2,
+    c: 4,
+  };
+let output = select(arr, obj);
+console.log( output); // --> { a: 1, c: 3 }
