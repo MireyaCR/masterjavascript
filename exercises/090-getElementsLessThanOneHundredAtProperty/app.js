e83de5ae8dff276083a2d5af520864f4036109f9
MireyaCR <mireya.mcr@gmail.com>
2022-09-11 19:33:20 +0000

    E090

--- exercises/090-getElementsLessThanOneHundredAtProperty/app.js
+++ exercises/090-getElementsLessThanOneHundredAtProperty/app.js
@@ -1,1 +1,18 @@
 // Write your function here
+function getElementsLessThan100AtProperty(obj,key) {
+    let arr = [];
+    if (obj[key] && typeof obj[key] === 'object' && obj[key].length) {
+        for (let index = 0; index < obj[key].length; index++) {
+            const element = obj[key][index];
+            if (element < 100) {
+                arr.push(obj[key][index]);
+            }
+        }
+    }
+    return arr;
+}
+let obj = {
+    key: [1000, 20, 50, 500]
+  };
+  let output = getElementsLessThan100AtProperty(obj, 'key');
+  console.log(output); // --> [20, 50]
