cc838c3b450367071b8b75948d6bbe4e25a662d7
MireyaCR <mireya.mcr@gmail.com>
2022-09-11 16:38:17 +0000

    E084

--- exercises/084-areValidCredentials/app.js
+++ exercises/084-areValidCredentials/app.js
@@ -1,1 +1,10 @@
 // Write your function here
+function areValidCredentials(nom, cseña) {
+    var comprueba = false; 
+    if (nom.length > 3 && cseña.length >=8){
+        comprueba = true;
+       }
+    return comprueba; 
+}
+let output = areValidCredentials('Ritu', 'mylongpassword')
+console.log(output); // --> verdadero
