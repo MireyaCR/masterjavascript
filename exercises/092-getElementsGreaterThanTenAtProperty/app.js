e6d3e59ea7adb28274b4d51faf168ab8dcb66023
MireyaCR <mireya.mcr@gmail.com>
2022-10-09 18:16:36 +0000

    masterjavascript

--- exercises/092-getElementsGreaterThanTenAtProperty/app.js
+++ exercises/092-getElementsGreaterThanTenAtProperty/app.js
@@ -1,1 +1,16 @@
 // Write your function here
+function getElementsGreaterThan10AtProperty(obj, key) {
+   
+     let array =obj.map(element => {(element && typeof element === 'object' && element.lenght)?
+          arrayvacio= obj[key].filter(value => value > 10) : arrayvacio=[]
+});
+    //Si el array no contiene ningún elemento mayor a 10, debería retornar una array vacío.
+    //Si la propiedad en esa key dada no es un array, debería retornar una array vacío.
+    //Si no hay ninguna propiedad en dicha key, debería retornar una array vacío.
+    return arrayvacio;
+}
+let obj = {
+    key: [1, 20, 30]
+  };
+  let output = getElementsGreaterThan10AtProperty(obj, 'key');
+  console.log(output); // --> [20, 30]
